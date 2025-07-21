9ad33d66cf42722c4600ad1714ecfe16a4a02a44
rajwindersxxx <rajwindersxxx@gmail.com>
2025-07-21 14:45:37 +0530

    Refactor imports in App component to use lazy loading for improved performance

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,15 @@
-import Features from "./Features";
-import Footer from "./Footer";
-import Header from "./Header";
-import Hero from "./Hero";
-// import MovingLogo from "./MovingLogo";
-import SessionCallToAction from "./SessionCallToAction";
-import SessionContact from "./SessionContact";
-import SessionService from "./SessionService";
-import SessionTestimonials from "./SessionTestimonials";
-import SocialIcons from "./SocialIcons";
+import { lazy } from "react";
+
+const Features = lazy(() => import("./Features"));
+const Footer = lazy(() => import("./Footer"));
+const Header = lazy(() => import("./Header"));
+const Hero = lazy(() => import("./Hero"));
+// const MovingLogo = lazy(() => import("./MovingLogo"));
+const SessionCallToAction = lazy(() => import("./SessionCallToAction"));
+const SessionContact = lazy(() => import("./SessionContact"));
+const SessionService = lazy(() => import("./SessionService"));
+const SessionTestimonials = lazy(() => import("./SessionTestimonials"));
+const SocialIcons = lazy(() => import("./SocialIcons"));
 
 export default function App() {
   return (
